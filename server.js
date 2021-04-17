ded822fd8fd83ee95901c0f2e2b47e4ba60bee47
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-04-16 23:34:25 -0700

    SERVER

--- server.js
+++ server.js
@@ -3,13 +3,33 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+
+const PORT= process.env.port|| 3001;
+
 const dotenv = require("dotenv");
 
 
 dotenv.config()
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/MY-SOCIAL-API', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
-app.listen(8800,() =>{
+
+
+
+
+
+
+app.listen(3301,() =>{
     console.log("we are working here!!!")
 })
