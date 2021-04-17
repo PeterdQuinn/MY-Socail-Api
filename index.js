185252d92abe0727c3e948b3449e93b0415e28f2
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-04-16 22:53:49 -0700

    more packages

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const helmet = require("helmet");
+const morgan = require ("morgan");
+
+
+dotenv.config()
+
+
+
+app.listen(8800,() =>{
+    console.log("we are working here!!!")
+})
