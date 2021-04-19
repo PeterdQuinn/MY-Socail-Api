7d48bb9ca24e3e6ef38760f63d562cb96864a828
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-04-18 20:43:52 -0700

    delete user

--- controlers/user-controller.js
+++ controlers/user-controller.js
@@ -44,5 +44,46 @@ getUserById({ params }, res) {
         });
     };
 
-    
+
+//creates user/////////// 
+createUser({ body}, res) {
+    User.create(body)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.status(400).json(err));
+};
+
+//updates User by id///
+updateUser({params, body}, res) {
+    User.findOneAndUpdate(
+        { _id: params.id}, 
+        body, 
+        { new: true, runValidators: true })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({message: 'No one with this  id!'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => res.status(400).json(err));
+};
+
+
+
+
+//// deltes the user id ///////
+
+deleteUser({params}, res) {
+    User.findOneAndDelete({ _id: params.id})
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'Not with that id!'})
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(400).json(err));
+
+};
+
+
     
